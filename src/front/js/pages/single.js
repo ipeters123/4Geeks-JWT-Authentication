c69a39d15a2a04c5188570084234da28398672fc
dariel0514 <dariel0514@hotmail.com>
2022-11-05 15:08:17 +0000

    Not Done

--- src/front/js/pages/single.js
+++ src/front/js/pages/single.js
@@ -1,13 +1,20 @@
 import React, { useState, useEffect, useContext } from "react";
+import PropTypes from "prop-types";
+import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
-import { Home } from "./home";
+import rigoImageUrl from "../../img/rigo-baby.jpg";
 
 export const Single = (props) => {
   const { store, actions } = useContext(Context);
+  const params = useParams();
 
   return (
     <div className="jumbotron">
-      {store.verifiedUser ? <h1>Authorized</h1> : <Home />}
+      <h1>Authenticaded</h1>
     </div>
   );
+};
+
+Single.propTypes = {
+  match: PropTypes.object,
 };
